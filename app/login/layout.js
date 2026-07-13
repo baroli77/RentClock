da6725aecfa6c27d681933a36b09abb9c3284dc7
baroli77 <obarton77@gmail.com>
2026-07-13 19:44:06 +0100

    Prevent login page indexing

---
+++ app/login/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  robots: { index: false, follow: false },
+  alternates: { canonical: "/login" },
+};
+
+export default function LoginLayout({ children }) {
+  return children;
+}
